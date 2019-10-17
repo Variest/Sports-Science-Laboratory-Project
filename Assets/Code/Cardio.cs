4430ea492b0013d7587ee8bbe2922fa22fc65aa7
AlexM814506 <56390864+AlexM814506@users.noreply.github.com>
2019-10-17 16:58:25 +0100

    everything

--- Assets/Code/Cardio.cs
+++ Assets/Code/Cardio.cs
@@ -7,61 +7,60 @@ using UnityEngine;
 public class Cardio : MonoBehaviour
 {
     //IMPORTANT INTEGERS
-    float Bla; //blood lactate -				measured - we're on to something
-    float BPd; //diastolic blood pressure -		PROBABLY INPUT - this tends not to change too much with exercise
-    float HR; //heart rate -					measured, but we have a decent way of calculating it;
-                                                //MEN - 4.7 BPM/10W
-                                                //WOMEN - 7.1 BPM/10W
-    float HRmax; //heart rate maximum =			(220-age)
-    float MAP; //mean arterial pressure =		(BPd + [0.3333(BPs-BPd)])
-    float OP; //oxygen pulse =					VO2/HR
-    float BPs; //systolic blood pressure -		measured, we have a DECENT way of measuring it;
-                                                //MEN - (0.346*W + 135.76)
-                                                //WOMEN - (0.103*W + 155.72)
+    public float Bla; //blood lactate -				measured - we're on to something
+    public float BlaT; //blood lactate threshold -     85% of max heart rate or 75% of VO2Max
+    public float BPd; //diastolic blood pressure -		PROBABLY INPUT - this tends not to change too much with exercise
+    public float HR; //heart rate -					measured, but we have a decent way of calculating it;
+              //MEN - 4.7 BPM/10W
+              //WOMEN - 7.1 BPM/10W
+    public float HRmax; //heart rate maximum =			(220-age)
+    public float MAP; //mean arterial pressure =		(BPd + [0.3333(BPs-BPd)])
+    public float OP; //oxygen pulse =					VO2/HR
+    public float BPs; //systolic blood pressure -		measured, we have a DECENT way of measuring it;
+               //MEN - (0.346*W + 135.76)
+               //WOMEN - (0.103*W + 155.72)
 
     //OTHER STUFF
-    float CO; //cardiac output =				SV*HR OR MAP/TPR
-    float BG; //glucose -						measured - ???? (dont need this anyway tbh)
-    float HRres; //heart rate reserve =		    HRmax-HRresting
-    float BP; //mean blood pressure =			CO*TPR
-    float SV; //stroke volume =					EDV-ESV
-    float HRrest; //heart rate resting -		input
+    public float CO; //cardiac output =				SV*HR OR MAP/TPR
+    public float BG; //glucose -						measured - ???? (dont need this anyway tbh)
+    public float HRres; //heart rate reserve =		    HRmax-HRresting
+    public float BP; //mean blood pressure =			CO*TPR
+    public float SV; //stroke volume =					EDV-ESV
+    public float HRrest; //heart rate resting -		input
 
     //LESS IMPORTANT
-    float EF; //ejection fraction =				(SV/EDV)*100
-    float EDV; //end diastolic volume -			measured - MIGHT BE INPUT? (changes a little bit)
-    float ESV; //end systolic volume -			measured - MIGHT BE INPUT? (changes a little bit)
-    float SW; //stroke work =					SV*MAP
-    float TPR; //total peripheral resistance =	MAP/CO
-   
+    public float EF; //ejection fraction =				(SV/EDV)*100
+    public float EDV; //end diastolic volume -			measured - MIGHT BE INPUT? (changes a little bit)
+    public float ESV; //end systolic volume -			measured - MIGHT BE INPUT? (changes a little bit)
+    public float SW; //stroke work =					SV*MAP
+    public float TPR; //total peripheral resistance =	MAP/CO
+
     //level one is entirely self contained, aside from oxygen pulse needing VO2 from a different section
     //levels two and three are very codependent, however, with them needing variables from eachother
 
-    CharacterCustomiser character;//declares character script
-    Pulmonaryvents vents;//declares vents script
+    CharacterCustomiser character; //declares character script
+    pvEquations vents; //declares vents script
     Bike bike;// declares bike script
-    //VO2 and AGE/GENDER and Wattage
-
- 
+              //VO2 and AGE/GENDER and Wattage
 
     public void Start()
-    {//sets scripts to variables to allow them to be connected.
-    character = GetComponent<CharacterCustomiser>();
-    vents = GetComponent<Pulmonaryvents>();
-    bike = GetComponent<Bike>();  
-
+    {
+        //sets scripts to variables to allow them to be connected.
+        character = GetComponent<CharacterCustomiser>();
+        vents = GetComponent<pvEquations>();
+        bike = GetComponent<Bike>();
     }
 
     //FUNCTIONS LEVEL 1
 
     void BPsfunction()
     {
-        if(character.gender == true)
+        if (character.gender == true)
         {
             BPs = (0.346f * bike.wattage + 135.76f);
         }
 
-        else if(character.gender == false)
+        else if (character.gender == false)
         {
             BPs = (0.103f * bike.wattage + 155.72f);
         }
@@ -79,14 +78,25 @@ public class Cardio : MonoBehaviour
 
     void HRfunction()
     {
-        if(character.gender == true)
+        if (character.gender == true)
         {
             HR = (4.7f * bike.wattage) / 10;
         }
 
-        else if(character.gender == false)
+        else if (character.gender == false)
         {
             HR = (7.1f * bike.wattage) / 10;
+        }
+
+        if (HR >= HRmax)
+        {
+            HR = HRmax;
+            //DANGER! DANGER!
+        }
+
+        if (HR >= BlaT)
+        {
+            //IT'S STARTING TO HURT, BL RISES
         }
     }
 
@@ -103,6 +113,11 @@ public class Cardio : MonoBehaviour
     void HRmaxfunction()
     {
         HRmax = (220 - character.age);
+    }
+
+    void BlaTfunction()
+    {
+        BlaT = (HRmax * 0.85f);
     }
 
     //FUNCTIONS LEVEL 2
