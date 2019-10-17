be670cb0a1015a885b6d66b84692380dc454e46b
AlexM814506 <56390864+AlexM814506@users.noreply.github.com>
2019-10-17 12:42:03 +0100

    gastemp

--- Assets/Code/Cardio.cs
+++ Assets/Code/Cardio.cs
@@ -2,20 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-}
 
 // Cardiovascular Functions.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
