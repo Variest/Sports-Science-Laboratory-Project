fccc660c5eab82457b95ce035353c45c6e46767d
AlexM814506 <56390864+AlexM814506@users.noreply.github.com>
2019-10-17 12:56:42 +0100

    calculating diastolicbp

--- Assets/Code/Cardio.cs
+++ Assets/Code/Cardio.cs
@@ -8,16 +8,16 @@ public class Cardio : MonoBehaviour
 {
     //IMPORTANT INTEGERS
     float Bla; //blood lactate -				measured - we're on to something
-    float BPd; //diastolic blood pressure -		PROBABLY INPUT - this tends not to change with exercise
+    float BPd; //diastolic blood pressure -		PROBABLY INPUT - this tends not to change too much with exercise
     float HR; //heart rate -					measured, but we have a decent way of calculating it;
-              //MEN - 4.7 BPM/10W
-              //WOMEN - 7.1 BPM/10W
+                                                //MEN - 4.7 BPM/10W
+                                                //WOMEN - 7.1 BPM/10W
     float HRmax; //heart rate maximum =			(220-age)
     float MAP; //mean arterial pressure =		(BPd + [0.3333(BPs-BPd)])
     float OP; //oxygen pulse =					VO2/HR
     float BPs; //systolic blood pressure -		measured, we have a DECENT way of measuring it;
-               //MEN - (0.346*W + 135.76)
-               //WOMEN - (0.103*W + 155.72)
+                                                //MEN - (0.346*W + 135.76)
+                                                //WOMEN - (0.103*W + 155.72)
 
     //OTHER STUFF
     float CO; //cardiac output =				SV*HR OR MAP/TPR
@@ -49,8 +49,15 @@ public class Cardio : MonoBehaviour
 
     void BPsfunction(float BPsfunc)
     {
-        BPs = BPsfunc;
-        //POTENTIALLY  if(character.gender == true){ BPs = (0.346*bike.W + 135.76)} OR  else if(character.gender == false){BPs = (0.103*bike.W + 155.72)};
+        if(character.gender == true)
+        {
+            BPs = (0.346 * bike.wattage + 135.76);
+        }
+
+        else if(character.gender == false)
+        {
+            BPs = (0.103 * bike.wattage + 155.72);
+        }
     }
 
     void BPdfunction(float BPdfunc)
@@ -65,8 +72,15 @@ public class Cardio : MonoBehaviour
 
     void HRfunction(float HRfunc)
     {
-        HR = HRfunc;
-        //POTENTIALLY if(character.gender == true){ HR = (4.7*bike.W)/10} else if(character.gender == false){HR = (7.1*bike.W)/10};
+        if(character.gender == true)
+        {
+            HR = (4.7 * bike.wattage) / 10;
+        }
+
+        else if(character.gender == false)
+        {
+            HR = (7.1 * bike.W) / 10;
+        }
     }
 
     void MAPfunction()
