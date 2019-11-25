244fb743f79dbaf061b29415a4b2354e3e81862a
SamHunt98 <49485927+SamHunt98@users.noreply.github.com>
2019-11-25 13:05:34 +0000

    Character Avatar Code and updated pv equations

--- Assets/Code/CharacterCustomiser.cs
+++ Assets/Code/CharacterCustomiser.cs
@@ -5,34 +5,65 @@ using UnityEngine.UI;
 
 public class CharacterCustomiser : MonoBehaviour
 {
-        public bool gender; //false for female, true for male
-        public int race = 0; //0 = white, 1 = black, 2 = asian
+        public int gender = 0; //drop down gives int values, 0 = FEMALE 1 = MALE
+        public int race = 0; //0 = WHITE, 1 = BLACK, 2 = ASIAN
         public int age = 16;
-        public float height = 0.0f; //PLEASE PUT THIS IN CENTIMETRES
+        public float height = 0.0f;
         public float weight = 0.0f;
         //Class Variables
         bool swimwear = false;
-        bool gasMarkOn = false;
+        bool gasMaskOn = false;
 
+    public Dropdown ddSex; //drop down box that allows the user to change the sex of the avatar
+    public Dropdown ddRace; //drop down box that allows the user to change the race of the avatar
+    public InputField ageInput; //input box that will get the age value of the avatar 
+    public InputField weightInput; //input box that will get the weight value of the avatar 
+    public InputField heightInput; //input box that will get the height value of the avatar 
+
+    public GameObject avatar;
+    public CharacterAvatar avatarVariables;
     public Button ChangeAvatar;
+    //we use these variables to set a minimum and maximum age value for the user to enter. 16 currently min, will ask for a max value from mitch
+    public float minAge = 16;
+    public float maxAge;
+    //same as before, setting max and min variables for the height, will ask mitch for sensible parameters 
+    public float minHeight;
+    public float maxHeight;
+    //max and min weight
+    public float minWeight;
+    public float maxWeight;
+
     // Start is called before the first frame update
     void Start()
-    {  
-        //Declares and sets the button
-        Button btn = ChangeAvatar.GetComponent<Button>();
+    {   //Declares and sets the button
+        Button btn = GetComponent<Button>();
+        //avatar = GameObject.Find("Avatar");
+        avatarVariables = avatar.GetComponent<CharacterAvatar>();
         btn.onClick.AddListener(TaskOnClick);
     }
 
     // Update is called once per frame
-    void Update(){}
+    void Update()
+    {
+          
+    }
 
     void TaskOnClick()
     {//What happens when the button is clicked
-        if(gender == false)//compares gender and then runs a switch to determine avatar type.
+        gender = ddSex.value;
+        avatarVariables.gender = gender;
+        race = ddRace.value;
+        avatarVariables.race = race;
+        avatarVariables.age = Mathf.Clamp(float.Parse(ageInput.text), minAge, maxAge);
+        avatarVariables.weight = Mathf.Clamp(float.Parse(weightInput.text), minWeight, maxWeight);
+        avatarVariables.height = Mathf.Clamp(float.Parse(heightInput.text), minHeight, maxHeight);
+
+        if (gender == 0)//compares gender and then runs a switch to determine avatar type.
         {
             switch (race)
             {
                 case 0:
+                    
                     Debug.Log("White Female");
                     break;
                 case 1:
@@ -61,6 +92,36 @@ public class CharacterCustomiser : MonoBehaviour
                 default:
                     break;
             }
+        }
+        if(avatarVariables.gender == 0 & avatarVariables.race == 0)
+        {
+            //white female
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteWFemale;
+        }
+        else if (avatarVariables.gender == 0 & avatarVariables.race == 1)
+        {
+            //black female
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteBFemale;
+        }
+        else if (avatarVariables.gender == 0 & avatarVariables.race == 2)
+        {
+            //asian female
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteAFemale;
+        }
+        else if (avatarVariables.gender == 1 & avatarVariables.race == 0)
+        {
+            //white male
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteWMale;
+        }
+        else if (avatarVariables.gender == 1 & avatarVariables.race == 1)
+        {
+            //black male
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteBMale;
+        }
+        else if (avatarVariables.gender == 1 & avatarVariables.race == 2)
+        {
+            avatarVariables.spriteRenderer.sprite = avatarVariables.spriteAMale;
+            //asian male
         }
     }
 }
--- Assets/Code/pvEquations.cs
+++ Assets/Code/pvEquations.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class pvEquations : MonoBehaviour
 {
 
+    [Header("Character")]
+    //will probably have a lot of these pulmonary variables within the character eventually, this is just a basic thing for now so that i can test some stuff
+    CharacterAvatar avatar;
+    [Space(10)]
     [Header("Values used for inspiration and expiration of air")]
-
     public float TI; //inspiratory time
     public float TE; //expiratory time
     public float TITE; //result for TI/TE
     public float breathTime; //total breath time
-    
+
+
     [Space(10)]
     [Header("Fractional Concentrations")]
-
     public float FECO2; //fractional concentration of expired carbon dioxide
     public float FICO2; //fractional concentration of inspired carbon dioxide 
     public float FEO2; //fractional concentration of expired oxygen
     public float FIO2; //fractional concentration of inspired oxygen
 
     [Space(10)]
+
     [Header("Minute Ventilation")]
+
 
     public float veATPS; //minute ventilation in an ATPS environment
     public float veSTPD; //minute ventilation in an STPD environment
@@ -40,10 +45,12 @@ public class pvEquations : MonoBehaviour
 
     public float VI; //used to calculate some stuff idk
     public float VO2; //value for oxygen consumption
+    public float VO2maxAge;
+    public float VO2maxHeight;
+    public float VO2maxWeight;
     public float VO2fr; //oxygen breath value
     public float VCO2; //carbon dioxide output
     public float EPOC; //excess post-exercise oxygen consumption
-
     [Space(10)]
     [Header("End-Tidal Partial Pressures")]
 
@@ -52,14 +59,11 @@ public class pvEquations : MonoBehaviour
 
     [Space(10)]
     [Header("Respiratory variables")]
-
     public float fr; //respiratory rate
     public float RER; //respiratory exchange ratio
     public float RQ; //respiratory quotient
-
     [Space(10)]
     [Header("Ventilatory variables")]
-
     public float Vecap; //ventilatory capacity
     public float VeVO2; //ventilatory equivalent for oxygen
     public float VeVCO2; //ventilatory equivalent for carbon dioxide
@@ -67,112 +71,153 @@ public class pvEquations : MonoBehaviour
 
     [Space(10)]
     [Header("Work Rate")]
-
     public float W; //work rate
-
-
     public float ExpireTime()
     {
         //used to calculate the value for Expiratory time, or TE
         //calculated as total breath time - TI
         //breathTime value is a placeholder for now as we may need to calculate it another way later in the project, this is just to get some stuff written down
 
-        TE = breathTime - TI;
-        return TE;
+        avatar.TE = avatar.breathTime - avatar.TI;
+        return avatar.TE;
 
     }
     public float InspireExpireRatio()
     {
         //used to find the ratio of inspiratory to expiratory time 
         //equation used is TI/TE
-        TITE = TI / TE;
-        return TITE;
+        avatar.TITE = avatar.TI / avatar.TE;
+        return avatar.TITE;
     }
     public float CalcVE()
     {
-        VE = VT * fr;
-        return VE;
+        avatar.VE = avatar.VT * avatar.fr;
+        return avatar.VE;
     }
     public float CalcVeATPS(float vol, float t)
     {
-        veATPS = (vol / t) * 60;
-        return veATPS;
+        avatar.veATPS = (vol / t) * 60;
+        return avatar.veATPS;
     }
 
     public float CalcVeSTPD(float Pb, float PH2O, float expTemp)
     {
         //PH2O will be calculated from the water vapour pressure table she gave us depending on the gas temperature present
-        veSTPD = veATPS * (((Pb - PH2O) / 760) * (273 / 273 + expTemp));
-        return veSTPD;
+        avatar.veSTPD = avatar.veATPS * (((Pb - PH2O) / 760) * (273 / 273 + expTemp));
+        return avatar.veSTPD;
     }
 
     public float CalcVeBTPS(float Pb, float PH2O, float expTemp)
     {
-        veBTPS = veATPS * ((Pb - PH2O) / (Pb - 47.08f) * (310f) / (273f + expTemp));
-        return veBTPS;
+        avatar.veBTPS = avatar.veATPS * ((Pb - PH2O) / (Pb - 47.08f) * (310f) / (273f + expTemp));
+        return avatar.veBTPS;
     }
 
     public float calcVI()
     {
-        VI = veSTPD * (((1 - FEO2 - FECO2) / (1 - FIO2 - FICO2)) * FIO2) - (VE * FEO2);
+        // VI = veSTPD * (((1 - FEO2 - FECO2) / (1 - FIO2 - FICO2)) * FIO2) - (VE * FEO2);
+        //1 - FEO2 - FECO2) /  (1-FIO2 - FICO2
+        avatar.VI = (1 - avatar.FEO2 - avatar.FECO2) / (1 - avatar.FIO2 - avatar.FICO2); //new vi equation mitch gave us
         return VI;
     }
     public float calcVCO2()
     {
-        VCO2 = veSTPD * (FECO2 - FICO2);
+        avatar.VCO2 = avatar.veSTPD * (avatar.FECO2 - avatar.FICO2);
         return VCO2;
 
     }
     public float OxygenConsumption()
     {
-        VO2 = (VI * (FIO2 / 100)) - (veSTPD * (FEO2 / 100)); //values are divided by 100 as they must be expressed as decimals instead of percentages
+        //VO2 = (VI * (FIO2 / 100)) - (veSTPD * (FEO2 / 100)); //values are divided by 100 as they must be expressed as decimals instead of percentages
+        //v02 = VE STPD * (((1 - FEO2 - FECO2) /  (1-FIO2 - FICO2)) * FIO2) - (VE STPD * FEO2)
+        avatar.VO2 = avatar.veSTPD * (((1 - avatar.FEO2 - avatar.FECO2) / (1 - avatar.FIO2 - avatar.FICO2)) * avatar.FIO2) - (avatar.veSTPD * avatar.FEO2); //equation including the VI calculation within it
         return VO2;
     }
     public float respiratoryExRatio()
     {
-        RER = VCO2 / VO2;
+        avatar.RER = avatar.VCO2 / avatar.VO2;
         return RER;
     }
     public float respiratoryQuotient()
     {
-        RQ = VCO2 / VO2;
+        avatar.RQ = avatar.VCO2 / avatar.VO2;
         return RQ;
     }
     public float VentCapacity(float FEV1)
     {
-        Vecap = FEV1 * 40;
-        return Vecap;
+        avatar.Vecap = FEV1 * 35;
+        return avatar.Vecap;
         //the sheet says "FEV1 * 40 or FEV1 * 35" so I don't know which one she wants us to use here????????????
+        //apparently there is no set variable that changes whether we use 40 or 35, however 35 is apparently the more common number to use and as such shall be the one we use
     }
     public float VentEquivOxygen()
     {
-        VeVO2 = VE / VO2;
-        return VeVO2;
+        avatar.VeVO2 = avatar.VE / avatar.VO2;
+        return avatar.VeVO2;
     }
     public float VentEquivCO2()
     {
-        VeVCO2 = VE / VCO2;
-        return VeVCO2;
+        avatar.VeVCO2 = avatar.VE / avatar.VCO2;
+        return avatar.VeVCO2;
     }
     public float calcEPOC(float EPOCduration, float EPOCconsumed)
     {
         //the epoc oxygen consumed will have to be calculated elsewhere, placeholder for now
         //will need a rest state and values relating to resting
-        EPOC = EPOCconsumed - (VO2 * EPOCduration);
-        return EPOC;
+        avatar.EPOC = EPOCconsumed - (avatar.VO2 * EPOCduration);
+        return avatar.EPOC;
     }
     public float calcMET(float weight)
     {
-        float temp = VO2 * 1000;
+        float temp = avatar.VO2 * 1000;
         float temp2 = temp / weight; //converting the VO2 value from litres into ml/kg
-        MET = temp2 / 3.5f;
-        return MET;
+        avatar.MET = temp2 / 3.5f;
+        return avatar.MET;
 
     }
     public float OxygenBreath()
     {
-        VO2fr = VO2 / fr;
-        return VO2fr;
+        avatar.VO2fr = avatar.VO2 / avatar.fr;
+        return avatar.VO2fr;
     }
+    public float VO2MaxAge()
+    {
+        if(avatar.gender == 0)
+        {
+            //female equations
+            avatar.VO2maxAge = (42.83f - (0.371f * avatar.age));
+
+        }
+        else
+        {
+           avatar.VO2maxAge = (50.02f - (0.394f * avatar.age));
+        }
+        return avatar.VO2maxAge;
+
+
+    }
+
+    public float VO2MaxHeight()
+    {
+        if(avatar.gender == 0)
+        {
+            float tempIBW = (62.6f * avatar.height / 100) - 45.5f; //height is divided by 100 as the calculation uses meters and my input is currently in cm
+           avatar.VO2maxHeight = (avatar.VO2maxAge * tempIBW) / 1000;
+        }
+        else
+        {
+            float tempIBW = (71.6f * avatar.height / 100) - 51.8f; //height is divided by 100 as the calculation uses meters and my input is currently in cm
+            avatar.VO2maxHeight = (avatar.VO2maxAge * tempIBW) / 1000;
+        }
+        return avatar.VO2maxHeight;
+
+    }
+
+    public float VO2MaxWeight()
+    {
+        avatar.VO2maxWeight = (avatar.VO2maxHeight * 1000f) / avatar.weight; //avatar weight should be in kg
+        return avatar.VO2maxWeight;
+    }
+
 
 }
