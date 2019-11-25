9b06b698826dffa56536e713ef413592a4f63a40
AlexM814506 <56390864+AlexM814506@users.noreply.github.com>
2019-11-25 13:29:43 +0000

    setup

--- Assets/Code/Output.cs
+++ Assets/Code/Output.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Output : MonoBehaviour
 {
+    Cardio cardio;
+    //HEART RATE(0-1) AND BLOOD LACTATE(0-4)
+    //PAINED FACE, AND TIRED LOOKING
+    BodyHeat heat;
+    //HEAT(0-3) AND WATER CONTENT (0-4)
+    //HOT AND SWEATY AND THIRSTY
+    Timer timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cardio = GetComponent<Cardio>();
+        heat = GetComponent<BodyHeat>();
+        timer = GetComponent<Timer>();
     }
 
     // Update is called once per frame
