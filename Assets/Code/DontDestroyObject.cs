c4d3daf01899d4a6ab3018ec4a864776019fe2e8
SamHunt98 <49485927+SamHunt98@users.noreply.github.com>
2019-12-02 13:43:35 +0000

    Update DontDestroyObject.cs

--- Assets/Code/DontDestroyObject.cs
+++ Assets/Code/DontDestroyObject.cs
@@ -8,7 +8,8 @@ public class DontDestroyObject : MonoBehaviour
     //will be placed onto the character avatar holder because we want to keep the variables assigned to it between scenes - they will be reset otherwise and we will lose the gender/age etc
     void Start()
     {
-        
+        //any scripts that reference the character avatar script will need to find an object in the scene with the tag "Avatar_Holder" and get the component from it, as currently we will
+        //not be able to get the value by dragging and dropping as the avatar holder is technically not in any other scene until we are playing the game.
     }
     private void Awake()
     {
