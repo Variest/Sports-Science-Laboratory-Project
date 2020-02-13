0146fa73ab580e053f6bd9ffeaa9a88246e9d0f6
AlexM814506 <56390864+AlexM814506@users.noreply.github.com>
2020-02-13 13:37:10 +0000

    jiggled everything around
    sorry lol

--- Assets/Code/pvEquations.cs
+++ Assets/Code/pvEquations.cs
@@ -25,6 +25,8 @@ public class pvEquations : MonoBehaviour
     public float FICO2; //fractional concentration of inspired carbon dioxide 
     public float FEO2; //fractional concentration of expired oxygen
     public float FIO2; //fractional concentration of inspired oxygen
+    //THESE ALL APPEAR TO BE FIXED AT AROUND
+    //FECO - 2.5-4.0, FEO - 16-18,
 
     [Space(10)]
     [Header("Minute Ventilation")]
@@ -101,6 +103,8 @@ public class pvEquations : MonoBehaviour
         
     }
 
+                                    //INSPI/EXPO FUNCTIONS
+
     public float ExpireTime()
     {
         //used to calculate the value for Expiratory time, or TE
@@ -111,6 +115,7 @@ public class pvEquations : MonoBehaviour
         return avatar.TE;
 
     }
+
     public float InspireExpireRatio()
     {
         //used to find the ratio of inspiratory to expiratory time 
@@ -118,14 +123,24 @@ public class pvEquations : MonoBehaviour
         avatar.TITE = avatar.TI / avatar.TE;
         return avatar.TITE;
     }
+
+                                    //FRAC. CONC. FUNCTIONS
+
+
+
+
+                                    //MINUTE VENTILATION FUNCTIONS
+
     public float CalcVE()
     {
         avatar.VE = Lungs.VT * avatar.fr;
         return avatar.VE;
     }
+
     public float CalcVeATPS(float vol, float t)
     {
         avatar.veATPS = (vol / t) * 60;
+        //avatar.veatps = (VE/60)*60?
         return avatar.veATPS;
     }
 
@@ -142,6 +157,8 @@ public class pvEquations : MonoBehaviour
         return avatar.veBTPS;
     }
 
+                                //OXYGEN CONSUMPTION FUNCTIONS
+
     public float calcVI()
     {
         // VI = veSTPD * (((1 - FEO2 - FECO2) / (1 - FIO2 - FICO2)) * FIO2) - (VE * FEO2);
@@ -149,12 +166,14 @@ public class pvEquations : MonoBehaviour
         avatar.VI = (1 - avatar.FEO2 - avatar.FECO2) / (1 - avatar.FIO2 - avatar.FICO2); //new vi equation mitch gave us
         return VI;
     }
+
     public float calcVCO2()
     {
         avatar.VCO2 = avatar.veSTPD * (avatar.FECO2 - avatar.FICO2);
         return VCO2;
 
     }
+
     public float OxygenConsumption()
     {
         //VO2 = (VI * (FIO2 / 100)) - (veSTPD * (FEO2 / 100)); //values are divided by 100 as they must be expressed as decimals instead of percentages
@@ -162,60 +181,7 @@ public class pvEquations : MonoBehaviour
         avatar.VO2 = avatar.veSTPD * (((1 - avatar.FEO2 - avatar.FECO2) / (1 - avatar.FIO2 - avatar.FICO2)) * avatar.FIO2) - (avatar.veSTPD * avatar.FEO2); //equation including the VI calculation within it
         return VO2;
     }
-    public float respiratoryExRatio()
-    {
-        avatar.RER = avatar.VCO2 / avatar.VO2;
-        return RER;
-    }
-    public float respiratoryQuotient()
-    {
-        avatar.RQ = avatar.VCO2 / avatar.VO2;
-        return RQ;
-    }
-    public float VentCapacity(float FEV1)
-    {
-        avatar.Vecap = FEV1 * 35;
-        return avatar.Vecap;
-        //the sheet says "FEV1 * 40 or FEV1 * 35" so I don't know which one she wants us to use here????????????
-        //apparently there is no set variable that changes whether we use 40 or 35, however 35 is apparently the more common number to use and as such shall be the one we use
-    }
-    public float VentEquivOxygen()
-    {
-        avatar.VeVO2 = avatar.VE / avatar.VO2;
-        return avatar.VeVO2;
-    }
-    public float VentEquivCO2()
-    {
-        avatar.VeVCO2 = avatar.VE / avatar.VCO2;
-        return avatar.VeVCO2;
-    }
-    public float calcEPOC(float EPOCduration, float EPOCconsumed)
-    {
-        //the epoc oxygen consumed will have to be calculated elsewhere, placeholder for now
-        //will need a rest state and values relating to resting
-        avatar.EPOC = EPOCconsumed - (avatar.VO2 * EPOCduration);
-        return avatar.EPOC;
-    }
-    public float calcMET(float weight)
-    {
-        float temp = avatar.VO2 * 1000;
-        float temp2 = temp / weight; //converting the VO2 value from litres into ml/kg
-        avatar.MET = temp2 / 3.5f;
-        return avatar.MET;
 
-    }
-
-    public float BreathFreq()
-    {
-        avatar.fr = (10 + (40 * (cardio.HR / cardio.HRmax)));
-        return avatar.fr;
-    }
-
-    public float OxygenBreath()
-    {
-        avatar.VO2fr = avatar.VO2 / avatar.fr;
-        return avatar.VO2fr;
-    }
     public float VO2MaxAge()
     {
         if(avatar.gender == 0)
@@ -229,6 +195,12 @@ public class pvEquations : MonoBehaviour
            avatar.VO2maxAge = (50.02f - (0.394f * avatar.age));
         }
         return avatar.VO2maxAge;
+    }
+
+    public float OxygenBreath()
+    {
+        avatar.VO2fr = avatar.VO2 / avatar.fr;
+        return avatar.VO2fr;
     }
 
     public float VO2MaxHeight()
@@ -253,5 +225,67 @@ public class pvEquations : MonoBehaviour
         return avatar.VO2maxWeight;
     }
 
+    public float calcEPOC(float EPOCduration, float EPOCconsumed)
+    {
+        //the epoc oxygen consumed will have to be calculated elsewhere, placeholder for now
+        //will need a rest state and values relating to resting
+        avatar.EPOC = EPOCconsumed - (avatar.VO2 * EPOCduration);
+        return avatar.EPOC;
+    }
 
+                                        //ET PP FUNCTIONS
+
+
+
+
+                                        //RESPIRATORY FUNCTIONS
+
+    public float respiratoryExRatio()
+    {
+        avatar.RER = avatar.VCO2 / avatar.VO2;
+        return RER;
+    }
+
+    public float respiratoryQuotient()
+    {
+        avatar.RQ = avatar.VCO2 / avatar.VO2;
+        return RQ;
+    }
+
+    public float BreathFreq()
+    {
+        avatar.fr = (10 + (40 * (cardio.HR / cardio.HRmax)));
+        return avatar.fr;
+    }
+
+                                        //VENTILATORY FUNCTIONS
+
+    public float VentCapacity(float FEV1)
+    {
+        avatar.Vecap = FEV1 * 35;
+        return avatar.Vecap;
+        //the sheet says "FEV1 * 40 or FEV1 * 35" so I don't know which one she wants us to use here????????????
+        //apparently there is no set variable that changes whether we use 40 or 35, however 35 is apparently the more common number to use and as such shall be the one we use
+    }
+
+    public float VentEquivOxygen()
+    {
+        avatar.VeVO2 = avatar.VE / avatar.VO2;
+        return avatar.VeVO2;
+    }
+
+    public float VentEquivCO2()
+    {
+        avatar.VeVCO2 = avatar.VE / avatar.VCO2;
+        return avatar.VeVCO2;
+    }
+
+    public float calcMET(float weight)
+    {
+        float temp = avatar.VO2 * 1000;
+        float temp2 = temp / weight; //converting the VO2 value from litres into ml/kg
+        avatar.MET = temp2 / 3.5f;
+        return avatar.MET;
+
+    }
 }
