00419b32c9fc389463a3492fbb122573a3e80cb7
Burkkake11 <UP780399@uni.ds.port.ac.uk>
2020-02-20 15:00:29 +0000

    Main Scene Adjustments
    Updated body type test and stopwatch updates

--- Assets/Code/TestInput.cs
+++ Assets/Code/TestInput.cs
@@ -41,16 +41,30 @@ public class TestInput : MonoBehaviour {
     public InputField MET;
     public InputField VO2fr;
 
-    public float testtime =0;
+    
     public Dropdown tempChoser; // drop down list currently used to experiment with having the program take in different functions
 
     public GameObject calcScripts;
 
     public Text Stopwatch;
+    public Text StopwatchMinutes;
+    public Text StopwatchHours;
     public Button playPause;
     public Button reset;
-    private bool play = true;
-
+    public Button increaseTimeSpeed;
+    public Button decreaseTimeSpeed;
+    private bool play = false;
+    public float timeSpeed = 1.0f;
+    public float testtime = 0.0f;
+    private float timerCalculations;
+    float hours, minutes, seconds;
+    float remainingSeconds;
+    float secondsInHour = 3600;
+    float secondsInMinute = 60;
+    double minuteRounder;
+    private int minutesInt;
+    double hourRounder;
+    private int hoursInt;
     public int functionChosen; //int value depending on which function you are trying to find the answer to
     // Use this for initialization
     void Start () {
@@ -71,8 +85,12 @@ public class TestInput : MonoBehaviour {
         MET.readOnly = true;
         VO2fr.readOnly = true;
         Stopwatch.GetComponent<Text>();
+        StopwatchMinutes.GetComponent<Text>();
+        StopwatchHours.GetComponent<Text>();
         playPause.onClick.AddListener(PlayPause);
         reset.onClick.AddListener(Reset);
+        increaseTimeSpeed.onClick.AddListener(plusTime);
+        decreaseTimeSpeed.onClick.AddListener(minusTime);
     }
 	
 	// Update is called once per frame
@@ -80,9 +98,12 @@ public class TestInput : MonoBehaviour {
 
         if(play == true)
         {
-            testtime += Time.deltaTime;//constantly updates testtime based off of runtime.
-            Stopwatch.text = testtime.ToString("0.00");
-            Debug.Log("help");
+            calculateTime();
+            System.Math.Floor(hourRounder);
+            System.Math.Floor(minuteRounder);
+            Stopwatch.text = seconds.ToString("00.00");
+            StopwatchMinutes.text = minutesInt.ToString("00");
+            StopwatchHours.text = hoursInt.ToString("00");
         }
         
 
@@ -93,9 +114,51 @@ public class TestInput : MonoBehaviour {
         play ^= true;
     }
 
+    void calculateTime()
+    {
+        testtime += Time.deltaTime * timeSpeed;//constantly updates testtime based off of runtime.
+        timerCalculations = testtime;
+        hours = timerCalculations / secondsInHour;
+        hourRounder = (double)hours;
+        hoursInt = (int)hourRounder;
+        
+        remainingSeconds = timerCalculations % secondsInHour;
+        minutes = remainingSeconds/secondsInMinute;
+        minuteRounder = (double)minutes;
+        minutesInt = (int)minuteRounder;
+
+        remainingSeconds = remainingSeconds % secondsInMinute;
+        seconds = remainingSeconds;
+    }
+
     void Reset()
     {
         testtime = 0;
+        minuteRounder = 0;
+        hourRounder = 0;
+        timerCalculations = 0;
+        remainingSeconds = 0;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
+        minutesInt = 0;
+        hoursInt = 0;
+        timeSpeed = 1.0f;
+        Stopwatch.text = seconds.ToString("00.00");
+        StopwatchMinutes.text = minutes.ToString("00");
+        StopwatchHours.text = hours.ToString("00");
+    }
+
+    void plusTime()
+    {
+        timeSpeed = timeSpeed + 1f;
+        Debug.Log(timeSpeed);
+    }
+
+    void minusTime()
+    {
+        timeSpeed = timeSpeed - 1f;
+        Debug.Log(timeSpeed);
     }
     public void Calculate()
     {
